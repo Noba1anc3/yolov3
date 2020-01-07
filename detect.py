fd4dade9dbdeaab900b9e4f36041149792768c73
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2020-01-07 15:29:59 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -3,145 +3,98 @@ from __future__ import division
 from models import *
 from utils.utils import *
 from utils.datasets import *
+from utils.parse_config import *
 
-import os
-import sys
-import time
-import datetime
-import argparse
-
-from PIL import Image
 from logzero import logger
+import argparse
+import tqdm
 
 import torch
 from torch.utils.data import DataLoader
-from torchvision import datasets
 from torch.autograd import Variable
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from matplotlib.ticker import NullLocator
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--image_folder", type=str, default="data/custom/images", help="path to dataset")
-    parser.add_argument("--model_def", type=str, default="config/yolov3-custom.cfg",
-                        help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="checkpoints/model.pth",
-                        help="path to weights file")
-    parser.add_argument("--class_path", type=str, default="data/custom/classes.names", help="path to class label file")
-    parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
-    parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
-    parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
-    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-    parser.add_argument("--checkpoint_model", type=str, help="path to checkpoint model")
-    opt = parser.parse_args()
-    print(opt)
+def evaluate(model, img_path, label_path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
+    model.eval()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    os.makedirs("output", exist_ok=True)
-
-    # Set up model
-    model = Darknet(opt.model_def, img_size=opt.img_size).to(device)
-
-    if opt.weights_path.endswith(".weights"):
-        # Load darknet weights
-        model.load_darknet_weights(opt.weights_path)
-    else:
-        # Load checkpoint weights
-        model.load_state_dict(torch.load(opt.weights_path, map_location=device))
-
-    model.eval()  # Set in evaluation mode
-
-    dataloader = DataLoader(
-        ImageFolder(opt.image_folder, img_size=opt.img_size),
-        batch_size=opt.batch_size,
-        shuffle=False,
-        num_workers=opt.n_cpu,
+    dataset = ListDataset(img_path, label_path, img_size=img_size, augment=False, multiscale=False)
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn
     )
-
-    classes = load_classes(opt.class_path)  # Extracts class labels from file
 
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
-    imgs = []  # Stores image paths
-    img_detections = []  # Stores detections for each image index
+    labels = []
+    sample_metrics = []  # List of tuples (TP, confs, pred)
+    for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):
+        # Extract labels
+        labels += targets[:, 1].tolist()
 
-    print('')
-    logger.info("Performing object detection:")
+        # Rescale target
+        targets[:, 2:] = xywh2xyxy(targets[:, 2:])
+        targets[:, 2:] *= img_size
 
-    prev_time = time.time()
-    for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
-        # Configure input
-        input_imgs = Variable(input_imgs.type(Tensor))
+        imgs = Variable(imgs.type(Tensor), requires_grad=False)
 
-        # Get detections
         with torch.no_grad():
-            detections = model(input_imgs)
-            detections = non_max_suppression(detections, opt.conf_thres, opt.nms_thres)
+            outputs = model(imgs)
+            outputs = non_max_suppression(outputs, conf_thres=conf_thres, nms_thres=nms_thres)
 
-        # Log progress
-        current_time = time.time()
-        inference_time = datetime.timedelta(seconds=current_time - prev_time)
-        prev_time = current_time
-        logger.info("\t+ Batch %d, Inference Time: %s" % (batch_i, inference_time))
+        sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=iou_thres)
 
-        # Save image and detections
-        imgs.extend(img_paths)
-        img_detections.extend(detections)
+    # Concatenate sample statistics
+    true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
+    logger.info('')
+    logger.info(true_positives)
+    logger.info(pred_scores)
+    logger.info(pred_labels)
+    logger.info(labels)
+    logger.info('')
+    precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
 
-    # Bounding-box colors
-    cmap = plt.get_cmap("tab20b")
-    colors = [cmap(i) for i in np.linspace(0, 1, 20)]
+    return precision, recall, AP, f1, ap_class
 
-    print('')
-    logger.info("Saving images:")
+def test(img_path, anno_path):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", type=int, default=1, help="size of each image batch")
+    parser.add_argument("--model_def", type=str, default="config/yolov3-custom.cfg", help="path to model definition file")
+    parser.add_argument("--data_config", type=str, default="config/custom.data", help="path to data config file")
+    parser.add_argument("--weights_path", type=str, default="checkpoints/yolov3.pth", help="path to weights file")
+    parser.add_argument("--class_path", type=str, default="data/custom/classes.names", help="path to class label file")
+    parser.add_argument("--iou_thres", type=float, default=0.5, help="iou threshold required to qualify as detected")
+    parser.add_argument("--conf_thres", type=float, default=0.001, help="object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    opt = parser.parse_args()
 
-    # Iterate through images and save plot of detections
-    for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        logger.info("(%d) Image: '%s'" % (img_i, path))
+    data_config = parse_data_config(opt.data_config)
+    class_names = load_classes(data_config["names"])
 
-        # Create plot
-        img = np.array(Image.open(path))
-        plt.figure()
-        fig, ax = plt.subplots(1)
-        ax.imshow(img)
+    # Initiate model
+    model = Darknet(opt.model_def).to(device)
+    model.load_state_dict(torch.load(opt.weights_path,map_location=device))
 
-        # Draw bounding boxes and labels of detections
-        if detections is not None:
-            # Rescale boxes to original image
-            detections = rescale_boxes(detections, opt.img_size, img.shape[:2])
-            unique_labels = detections[:, -1].cpu().unique()
-            n_cls_preds = len(unique_labels)
-            bbox_colors = random.sample(colors, n_cls_preds)
-            for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
-                logger.info("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
+    logger.info("Compute mAP...")
 
-                box_w = x2 - x1
-                box_h = y2 - y1
+    precision, recall, AP, f1, ap_class = evaluate(
+        model,
+        img_path = img_path,
+        label_path = anno_path,
+        iou_thres=opt.iou_thres,
+        conf_thres=opt.conf_thres,
+        nms_thres=opt.nms_thres,
+        img_size=opt.img_size,
+        batch_size=8,
+    )
 
-                color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
-                # Create a Rectangle patch
-                bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor="none")
-                # Add the bbox to the plot
-                ax.add_patch(bbox)
-                # Add label
-                plt.text(
-                    x1,
-                    y2,
-                    s=classes[int(cls_pred)],
-                    color="white",
-                    verticalalignment="top",
-                    bbox={"color": color, "pad": 0},
-                )
+    logger.info("Average Precisions:")
+    for i, c in enumerate(ap_class):
+        logger.info(f"+ Class '{c}' ({class_names[c]}) - AP: {AP[i]}")
 
-        # Save generated image with detections
-        plt.axis("off")
-        plt.gca().xaxis.set_major_locator(NullLocator())
-        plt.gca().yaxis.set_major_locator(NullLocator())
-        filename = path.split("/")[-1].split(".")[0]
-        plt.savefig(f"output/{filename}.png", bbox_inches="tight", pad_inches=0.0)
-        plt.close()
+    logger.info(f"mAP: {AP.mean()}")
+
+if __name__ == "__main__":
+    test('data/test/Image_test/','data/test/Anno_test/')
